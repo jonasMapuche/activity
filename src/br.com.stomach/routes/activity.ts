04d5a7ee15c914a277dc22bac3189e75936b60c2
Bertha Zatz <berthazatz@gmail.com>
2026-03-07 11:10:59 -0300

    Exclusão de classes e seus históricos
    Exclusão de classes e seus históricos

--- src/br.com.stomach/routes/activity.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { Controller } from "../controllers/controller";
-router.get('/', Controller.hello_physical);
-router.get('/all', Controller.getAll_physical);
-router.get('/framework/:id', Controller.getFramework_physical);
-router.get('/name/:id', Controller.getName_physical);
-router.post('/', Controller.save_physical);
-module.exports = router;
--- src/br.com.stomach/routes/artless.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { Controller } from "../controllers/controller";
-router.get('/', Controller.hello_history);
-router.get('/all', Controller.getAll_history);
-router.get('/name/:id', Controller.getName_history);
-router.post('/range', Controller.postRange_history);
-router.post('/', Controller.save_history);
-module.exports = router;
